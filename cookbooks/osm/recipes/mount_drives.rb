e37bc51c840298d6b4cdf6467a1575d9bc0652c6
radekg <radek@gruchalski.com>
2013-12-01 17:46:27 +0100

    Try new way of mounting drives, High I/O instances don't come with drives formatted / mounted.

--- cookbooks/osm/recipes/mount_drives.rb
+++ cookbooks/osm/recipes/mount_drives.rb
@@ -4,12 +4,31 @@ end
 
 ruby_block "mount_drives" do
   block do
+    fstab = []
     count = 1
-    `blkid | grep ext3`.split("\n").each do |line|
-      `mkdir -p /mnt#{count}`
-      `echo "#{line.split(':').first} /mnt#{count}  auto  defaults,nobootwait,comment=cloudconfig 0 2" >> /etc/fstab`
-      count = count + 1
-    end
+    desired_type = "ext4"
+    `fdisk -l 2>&1 | grep doesn`.split("\n").each{|line|
+            device = line.split(" ")[1]
+            uuid = `blkid -o value -s UUID #{device}`.chomp
+            type = `blkid -o value -s TYPE #{device}`.chomp
+            label = `blkid -o value -s LABEL #{device}`.chomp
+            puts "#{device} :: #{uuid} :: #{type} :: #{label}"
+            if uuid.empty? and label.empty? and type.empty?
+                    `sudo mkfs -t #{desired_type} #{device}`
+                    `mkdir -p /mnt#{count}`
+                    mount_point = "/mnt#{count}"
+                    fstab << "#{device}     /mnt#{count}  #{desired_type} defaults,nobootwait,comment=cloudconfig      0       2"
+            else
+                    # get mount point
+                    mount_point = `mount -l | grep #{device}`.chomp.split(" ")[1]
+                    options = ( mount_point == "/" ? "defaults" : "defaults,nobootwait,comment=cloudconfig" )
+                    fstab << "#{device}     #{mount_point}  #{type} #{options}      0       2"
+            end
+    }
+    `rm -Rf /etc/fstab`
+    fstab.each {|fstab_line|
+            `echo "#{fstab_line}" >> /etc/fstab`
+    }
     `mount -a`
   end
 end
